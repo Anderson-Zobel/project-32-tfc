33e56fd789597d441441231148323012658856c4
Anderson-Zobel <anderson.zobels@gmail.com>
2022-04-27 16:33:18 -0300

    feat: migration create-matches

---
+++ app/backend/src/database/migrations/20220427192259-create-matches.js
@@ -0,0 +1,60 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('matches', {
+      id: {
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+        type: Sequelize.INTEGER,
+      },
+
+      home_team: {
+        foreignKey: true,
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'teams',
+          key: 'id'
+        },
+
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+
+      },
+
+      home_team_goals: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+
+      away_team: {
+        foreignKey: true,
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'teams',
+          key: 'id'
+        },
+
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+
+      away_team_goals: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+
+      in_progression: {
+        allowNull: false,
+        type: Sequelize.BOOLEAN,
+      },
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('matches');
+  }
+};
