b0d4347bd7ee0a011994cf3ab3e12eabaf7302c5
Anderson-Zobel <anderson.zobels@gmail.com>
2022-05-03 08:35:12 -0300

    composer

--- app/backend/src/middlewares/login.validation.ts
+++ app/backend/src/middlewares/login.validation.ts
@@ -11,9 +11,9 @@ export default function login(req: Request, res: Response, next: NextFunction) {
     return res.status(400).json({ message: 'All fields must be filled' });
   }
 
-  // if (typeof email !== 'string' || typeof password !== 'string') {
-  //   return res.status(400).json({ message: 'email and password must be a string'});
-  // }
+  if (typeof email !== 'string' || typeof password !== 'string') {
+    return res.status(400).json({ message: 'email and password must be a string'});
+  }
 
   const emailVerify = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
